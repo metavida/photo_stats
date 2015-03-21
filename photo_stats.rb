529c75624dd94297b701ace21f5ab86920cc8bae
Marcos Wright-Kuhns <marcoswk@haikulearning.com>
2015-03-21 14:37:13 -0700

    Iterate over photo files

--- photo_stats.rb
+++ photo_stats.rb
@@ -1,6 +1,54 @@
 #!/usr/bin/env ruby
+require 'time'
 
 # Compile stats given a directoy of photos
-photo_dir = ARGV[0]
+class PhotoStats
+  include Enumerable
 
-puts photo_dir
+  attr_accessor :photo_dir
+
+  def initialize(photo_dir)
+    @photo_dir = File.expand_path(photo_dir)
+    ensure_dir_exists
+  end
+
+  def compile_stats
+    each do |file_path|
+      file = File.basename(file_path)
+      taken_at = Time.parse(file.gsub(
+        /(\d+)-(\d+)-(\d+) (\d+).(\d+).(\d+)\b.*/,
+        '\1/\2/\3 \4:\5:\6'
+      ))
+      puts taken_at
+    end
+  end
+
+  def each
+    Dir.glob(File.join(photo_dir, '**/*')).each do |file|
+      next if File.directory?(file)
+      yield(file)
+    end
+  end
+
+  private
+
+  def ensure_dir_exists
+    return if File.exists?(photo_dir)
+    warn "The given photo dir doesn't exist: #{photo_dir}"
+    exit 1
+  end
+end
+
+def usage
+  puts <<-USAGE
+usage: photo_stats.rb path_to_photo_dir
+USAGE
+end
+
+case ARGV.size
+when 1
+  stats = PhotoStats.new(ARGV[0])
+  stats.compile_stats
+else
+  usage
+end
